e1ce2e438e9e60e87266a09f24243ed58e5d5a5e
Yanai Yakuel <yanai.yakuel@gmail.com>
2025-04-03 22:28:08 +0300

    Update average.py

--- Algorithms & Encryption/average.py
+++ Algorithms & Encryption/average.py
@@ -1,1 +1,12 @@
+def average():
+    user_input = input("input list of numbers separate with spaces ")
+    list = []
+
+    for number in user_input.split():
+        list.append(float(number))
+    average = sum(list) / len(list)
+    round(average, 2)
+    print(f"The average is {average}")
+average()
+    
 
