2fbb660603a414dcef5b1abf589adbbbfa5af0a1
Yanai Yakuel <yanai.yakuel@gmail.com>
2026-03-03 12:41:17 +0200

    change time from 0.05 to 0.01

--- Password Security Tools/simple_password_cracking.py
+++ Password Security Tools/simple_password_cracking.py
@@ -7,7 +7,7 @@ print("craking password")
 for i in range(1000):
     guess = str(i).zfill(3)
     print("tyring", guess)
-    time.sleep(0.05)
+    time.sleep(0.01)
 
     if guess == password:
         print("password found")
